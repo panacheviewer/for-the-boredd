63244554b923eb76f4d4046696d576d3f6344144
panacheviewer <panacheviewer@gmail.com>
2025-12-31 17:24:53 +0100

    Update startPage.js

--- startPage.js
+++ startPage.js
@@ -1,4 +1,4 @@
-let currentCountdownDate = new Date("Feb 1, 2026 00:00:00").getTime();
+let currentCountdownDate = new Date("Jan 28, 2026 09:20:00").getTime();
 let specialSub = document.getElementById("specialSub");
 
 function time() {
@@ -9,7 +9,7 @@ function time() {
     let minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
-    specialSub.textContent = "Available For: " + days + "d " + String(hours).padStart(2, "0") + "h " + String(minutes).padStart(2, "0") + "m " + String(seconds).padStart(2, "0") + "s";; 
+    specialSub.textContent = "Available In: " + days + "d " + String(hours).padStart(2, "0") + "h " + String(minutes).padStart(2, "0") + "m " + String(seconds).padStart(2, "0") + "s";; 
 }
 time();
 
