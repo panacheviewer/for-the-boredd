bca6aa95133aed99b63a05fb9ee76b941a736dd6
panacheviewer <panacheviewer@gmail.com>
2026-01-01 18:45:16 +0100

    Update startPage.js

--- startPage.js
+++ startPage.js
@@ -1,5 +1,9 @@
-let currentCountdownDate = new Date("Jan 28, 2026 09:20:00").getTime();
-let specialSub = document.getElementById("specialSub");
+const currentCountdownDate = new Date("Jan 28, 2026 09:20:00").getTime();
+const specialSub = document.getElementById("specialSub");
+const ITPlay = document.getElementById('ITPlay');
+const devTag = document.getElementById('devTag');
+let kp = localStorage.getItem('kp') || false;
+
 
 function time() {
     let todayDate = new Date().getTime();
@@ -14,3 +18,32 @@ function time() {
 time();
 
 setInterval(time, 1000);
+
+ITPlay.onclick = function() {
+    if (kp === true) {
+        kp = true;
+        localStorage.setItem('kp', true);
+        window.location.href = './InternetTasks';
+    } else {
+        let pr;
+        pr = window.prompt('Enter Early Access Code: ');
+        if (pr === "00000000000001") {
+            kp = true;
+            localStorage.setItem('kp', true);
+            window.location.href = './InternetTasks';
+        } else {
+            window.alert('Failed To Access');
+        }
+
+    }
+
+}
+
+do {
+    devTag.style.visibility = "visible";
+} while (kp === true)
+
+do {
+    devTag.style.visibility = "hidden";
+} while (kp === false)
+
